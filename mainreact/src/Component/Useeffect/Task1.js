d1b58f1ebbd34d6d6a96ae797637585073a26fc5
rajankathiriya <rajankathiriya99@gmail.com>
2022-11-15 17:06:15 +0530

    data

--- mainreact/src/Component/Useeffect/Task1.js
+++ mainreact/src/Component/Useeffect/Task1.js
@@ -1,37 +1,51 @@
 import React from 'react'
-import { useEffect,useState } from 'react';
+import { useEffect, useState } from 'react';
+import './Task1.css'
 
 export default function Task() {
 
   const [data, setdata] = useState([]);
+  const [search, setsearch] = useState();
 
   useEffect(() => {
-   fetch("https://jsonplaceholder.typicode.com/users")
-   .then(r=>r.json())
-   .then(r=>{setdata(r)})
+    fetch("https://jsonplaceholder.typicode.com/users")
+      .then(r => r.json())
+      .then(r => { setdata(r) })
   }, []);
-  
-  const handler = (e) =>{
-    setdata(e.target.value)
-  }
 
-  const Click = () =>{
-
+  const handler = (e) => {
+    setsearch(e.target.value)
   }
 
   return (
     <div>
-      <input type="text" onChange={handler} value={data} />
-      <button onClick={Click}>Search</button>
+      <input type="text" onChange={handler} value={search} />
+      {/* <button onClick={Click} >Search</button><br /><br /> */}
 
-      {data.filter((value,index,array)=>{
-        return(value.name.indexOf(data) <=0)
-      })
+      <table>
+        <thead>
+          <tr>
+            <th>id</th>
+            <th>name</th>
+            <th>Username</th>
+          </tr>
+        </thead>
+        <tbody>
+          {data.filter((value, index, array) => {
+            return (value.name.indexOf(search) >= 0)
+          }).map((value) => {
+              return (
+                <tr>
+                  <td>{value.id}</td>
+                  <td>{value.name}</td>
+                  <td>{value.username}</td>
+                </tr>
+              )
+            })}
+        </tbody>
+      </table>
 
-      .map((value)=>{
-        return(<h5>{value.name}</h5>)
-        
-      })}
     </div>
   )
 }
+
